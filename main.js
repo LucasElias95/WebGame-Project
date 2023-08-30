7862cf8cd97ca9308f85f018d22d12ea06f62e7a
LucasElias95 <lucas.a.elias@gmail.com>
2023-08-30 15:11:43 -0300

    Desenvolvimento do meu webgame
    Desenvolvendo meu WebGame com linguagem HTML5 CSS3 e JS

---
+++ main.js
@@ -0,0 +1,14 @@
+function transferImage(clickedElement) {
+  const sourceImage = clickedElement.querySelector('img');
+  const imageUrl = sourceImage.src;
+
+  // Armazenar o URL da imagem no LocalStorage
+  localStorage.setItem('selectedImage', imageUrl);
+}
+const selectedImage = document.getElementById('selectedImage');
+const imageUrl = localStorage.getItem('selectedImage');
+if (imageUrl) {
+  selectedImage.src = imageUrl;
+}
+
+  
